3940bbba79b0d5a0b0c21785638c47252c83fa66
mohan <mohan.chanti4@gmail.com>
2019-08-02 14:40:47 +0530

    new changes

---
+++ src/main/java/org/com/app/mvnhh/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvnhh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvnhh" );
+    }
+}
